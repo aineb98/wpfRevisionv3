8be1bb19b8b1fe335c9b28e3602d30de9a81913c
s00176951 <s00176951@LAB-B1042-13>
2020-01-21 12:00:14 +0000

    last

--- wpfRevision/MainWindow.xaml.cs
+++ wpfRevision/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace wpfRevision
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
